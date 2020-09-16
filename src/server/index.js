50f37f4a8a64c5b63028d9f4d65b96c8537420fc
Toby Jorris <tobyjorris@gmail.com>
2020-09-16 10:36:20 -0500

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+app.use(cors());
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
