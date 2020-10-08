7d8358c57237a8d5fb702ac80921344e0088ae35
Toby Jorris <tobyjorris@gmail.com>
2020-10-07 20:48:24 -0500

    commit

--- src/client/js/postToServer.js
+++ src/client/js/postToServer.js
@@ -1,16 +1,17 @@
-const postDataToServer = async (url = '', data = {}) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data),
-    })
-    try {
-        return await response.json();
-    } catch (e) {
-        console.log('error', e);
-    }
-}
+import "babel-polyfill";
+const postDataToServer = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+  try {
+    return await response.json();
+  } catch (e) {
+    console.log("error", e);
+  }
+};
 
-export { postDataToServer }
+export { postDataToServer };
